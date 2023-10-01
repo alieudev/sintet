1137cbd3135a4d2dc68302daeff6796911e872b5
Alieu Baldeh <alieu.dev@gmail.com>
2023-10-01 01:25:14 -0400

    added homepage

---
+++ src/components/HomePage.js
@@ -0,0 +1,10 @@
+function HomePage() {
+    return(
+    <div>
+        <h2>Name: Alieu Baldeh</h2>
+        <p>Age: 30</p>
+        <p>Place of birth: Sintet</p>
+    </div>)
+}
+
+export default HomePage;
